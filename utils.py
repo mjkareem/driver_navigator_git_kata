8f4e8ebf913f6e9faddb36bdbc3735d7ab41fe9d
Poonam <you@example.com>
2023-04-17 15:51:41 +0200

    Added function

---
+++ utils.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+def load_passengers():
+    # Load the Titanic dataset
+    df = pd.read_csv('titanic.csv')
+
+    # Select only the female passengers
+    female_passengers = df[df['Sex'] == 'female']
+
+    return female_passengers
