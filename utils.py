f26d80c72fc2f0d2739616394ff28dd4d0247aa0
Poonam <you@example.com>
2023-04-18 13:29:52 +0200

    Add code for female pesseangers

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def load_passengers():
     # Load the Titanic dataset
-    df = pd.read_csv('titanic.csv')
+    df = pd.read_csv('data/titanic.csv')
 
     # Select only the female passengers
     female_passengers = df[df['Sex'] == 'female']
